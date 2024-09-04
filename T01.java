bdd9c1e3dda4a848e2cd7c511ffc286093cff7b1
SwastiSihombing <swastisihombing@gmail.com>
2024-09-04 21:27:15 +0700

    sudah selesai

--- T01.java
+++ T01.java
@@ -1,3 +1,19 @@
-// NIM - NAMA
-// NIM - NAMA
+// 12S24030 - Swasti Maristella Sihombing
+// 12S24037 - Maxwell Rumahorbo
+import java.util.*;
+import java.lang.Math;
+
+public class Program {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String namalengkap;
+
+        namalengkap = input.nextLine();
+        System.out.println(namalengkap);
+        // Type Name;
+
+    }
+}
+
 
